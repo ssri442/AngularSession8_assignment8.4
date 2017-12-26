326de78343bbee637e8114625e90e53f7c84ec12
Sai <ssri442@gmail.com>
2017-12-26 23:33:29 +0530

    commit5.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'my-app-component',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:  ` <div class="container">
+  <div id = login class="form-group">
+  <input type="text" class="form-control"
+  placeholder="FirstName"
+  [(ngModel)] ="FirstName">
+ 
+  <button type="button" class="btn btn-primary pull-center">Login </button>
+  </div>
+  </div>
+  `,
+   styleUrls: ['./app.component.css']
 })
 export class RootComponent {
   title = 'app';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { RootComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +9,10 @@ import { RootComponent } from './app.component';
     RootComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
+
   ],
   providers: [],
   bootstrap: [RootComponent]
