9e36339bbbec445b6b3190d66a70f433d49fbd3f
Sai <ssri442@gmail.com>
2017-12-26 20:54:21 +0530

    5.1 final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
-  selector: 'app-root',
+  selector: 'my-app-component',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class RootComponent {
   title = 'app';
+   
 }
